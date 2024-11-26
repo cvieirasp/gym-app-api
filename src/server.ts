b2b848a8eed10a51db8c8ae36690ec7e046e02ea
Carlos Figueiredo <cvieirasp@gmail.com>
2024-11-26 16:30:46 -0300

    created project structure

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import app from '@/app'
+import env from '@/env'
+
+app
+  .listen({
+    host: '0.0.0.0',
+    port: env.PORT,
+  })
+  .then((address) => {
+    console.log(`Server listening on ${address}`)
+  })
+  .catch((err) => {
+    console.error(err)
+    process.exit(1)
+  })
